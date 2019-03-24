5699b830bde713f976cd2648c6bdbe9cf036a935
Andrei Bogomja <bogomja@gmail.com>
2019-03-24 14:07:18 +0000

    Removes @Indexed annotation

--- src/main/java/com/otus/hw_11/domain/Genre.java
+++ src/main/java/com/otus/hw_11/domain/Genre.java
@@ -3,14 +3,12 @@ package com.otus.hw_11.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.Indexed;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Genre {
 
-    @Indexed
     private String genreName;
 
 }
