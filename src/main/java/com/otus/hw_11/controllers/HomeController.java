7cd0461c5209fdec00bfd86373f4f337f4ba81e7
Andrei Bogomja <bogomja@gmail.com>
2019-03-27 11:20:38 +0000

    Adds HomeController class

---
+++ src/main/java/com/otus/hw_11/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.otus.hw_11.controllers;
+
+import com.otus.hw_11.services.BookService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final BookService service;
+
+    @GetMapping({"", "/", "/home"})
+    public String index(final Model model) {
+        model.addAttribute("booksTotal", service.getBooksCount());
+        model.addAttribute("authorsTotal", service.getAuthorsCount());
+        model.addAttribute("genresTotal", service.getGenresCount());
+        return "index";
+    }
+
+}
