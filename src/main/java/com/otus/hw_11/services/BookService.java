bb84938c3bab7fdd6861868ac4bb92ad377fbb27
Andrei Bogomja <bogomja@gmail.com>
2019-03-27 11:19:37 +0000

    Adds book service class

---
+++ src/main/java/com/otus/hw_11/services/BookService.java
@@ -0,0 +1,26 @@
+package com.otus.hw_11.services;
+
+import com.otus.hw_11.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepo;
+
+    public Mono<Long> getAuthorsCount() {
+        return bookRepo.findDistinctAuthorLastName().count();
+    }
+
+    public Mono<Long> getGenresCount() {
+        return bookRepo.findDistinctGenres().count();
+    }
+
+    public Mono<Long> getBooksCount() {
+        return bookRepo.count();
+    }
+
+}
