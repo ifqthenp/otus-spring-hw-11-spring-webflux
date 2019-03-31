b4aa6278bd6b4260c4bc56d51b6dc329d01132b4
Andrei Bogomja <bogomja@gmail.com>
2019-03-31 12:20:44 +0100

    Adds solution with nested routes

--- src/main/java/com/otus/hw_11/rest/RouterFunctionConfig.java
+++ src/main/java/com/otus/hw_11/rest/RouterFunctionConfig.java
@@ -2,11 +2,13 @@ package com.otus.hw_11.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -14,12 +16,22 @@ public class RouterFunctionConfig {
 
     @Bean
     RouterFunction<ServerResponse> genreRoutes(final BookHandler handler) {
-        return route(GET("/rest/library/books").and(accept(APPLICATION_JSON)), handler::getAllBooks)
-            .andRoute(GET("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::getBook)
-            .andRoute(POST("/rest/library/books").and(accept(APPLICATION_JSON)), handler::saveBook)
-            .andRoute(PUT("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::updateBook)
-            .andRoute(DELETE("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::deleteBook)
-            .andRoute(DELETE("/rest/library/books").and(accept(APPLICATION_JSON)), handler::deleteAllBooks);
+        //        return route(GET("/rest/library/books").and(accept(APPLICATION_JSON)), handler::getAllBooks)
+        //            .andRoute(GET("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::getBook)
+        //            .andRoute(POST("/rest/library/books").and(accept(APPLICATION_JSON)), handler::saveBook)
+        //            .andRoute(PUT("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::updateBook)
+        //            .andRoute(DELETE("/rest/library/books/{id}").and(accept(APPLICATION_JSON)), handler::deleteBook)
+        //            .andRoute(DELETE("/rest/library/books").and(accept(APPLICATION_JSON)), handler::deleteAllBooks);
+
+        return nest(path("/rest/library/books"),
+            nest(accept(APPLICATION_JSON).or(contentType(APPLICATION_JSON)),
+                route(GET("/"), handler::getAllBooks)
+                    .andRoute(method(HttpMethod.POST), handler::saveBook)
+                    .andRoute(DELETE("/"), handler::deleteAllBooks)
+                    .andNest(path("/{id}"),
+                        route(method(HttpMethod.GET), handler::getBook)
+                            .andRoute(method(HttpMethod.PUT), handler::updateBook)
+                            .andRoute(method(HttpMethod.DELETE), handler::deleteBook))));
     }
 
 }
