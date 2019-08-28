5faf0c0f99de05a5ba30c8e759ee1d34c10b73e6
Andrei Bogomja <bogomja@gmail.com>
2019-08-28 21:05:14 +0100

    Added @Repository annotation

--- src/main/java/com/otus/hw_11/repositories/BookRepositoryCustom.java
+++ src/main/java/com/otus/hw_11/repositories/BookRepositoryCustom.java
@@ -1,7 +1,9 @@
 package com.otus.hw_11.repositories;
 
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface BookRepositoryCustom {
 
     Flux<String> findDistinctAuthorLastName();
